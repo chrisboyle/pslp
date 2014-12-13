817d40a25bce8a9dc7ba9b14ef79a51c3864f387
Michael Schlies <khalaan@gmail.com>
2014-12-12 21:26:39 -0600

    Fixed bug with edit activity after setting wireless
    Tested editing activity both with and without not checked, all power
    source types. No more force closed of the EditActivity screen

--- src/name/boyle/chris/powersource/EditActivity.java
+++ src/name/boyle/chris/powersource/EditActivity.java
@@ -84,13 +84,9 @@ public final class EditActivity extends Activity
 			if (forwardedBundle != null)
 			{
 				int source = forwardedBundle.getInt(Constants.BUNDLE_EXTRA_POWER_SOURCE, -1);
-                if (source == 3) {
-                    spinner.setSelection(source);
-                    source++;
-                } else
+                if (source == 4) source--;
 				if (source >= 0) spinner.setSelection(source);
 				notCB.setChecked(forwardedBundle.getBoolean(Constants.BUNDLE_EXTRA_NOT, false));
-                
             }
 		}
 		/*
